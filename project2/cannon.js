b072c15077bc22565420b40dda688d12cb54128b
Cruziper <goncalo.garcia.cruz@tecnico.ulisboa.pt>
2019-10-16 21:15:33 +0100

    Added AxesHelper to cannons

--- project2/cannon.js
+++ project2/cannon.js
@@ -8,6 +8,8 @@ export default class Cannon extends THREE.Group {
 		this.cannonMainSize = [5, 5, 20, 32];
 		this.cannonTireSize = [3, 2, 10, 16]; // Inner Radius [0]-[1]
 
+		this.cannonFINAL = new THREE.Group();
+
     const wallOffset = 50;
 
     this.cannon1Mainposition = [
@@ -35,12 +37,12 @@ export default class Cannon extends THREE.Group {
 			color: 0x434371,
 			wireframe: false,
 		});
-		this.createCannon(...this.cannon1Mainposition);
-    this.createCannon(...this.cannon2Mainposition);
-    this.createCannon(...this.cannon3Mainposition);
+		this.createCannon(this.cannonFINAL, ...this.cannon1Mainposition);
+    this.createCannon(this.cannonFINAL, ...this.cannon2Mainposition);
+    this.createCannon(this.cannonFINAL, ...this.cannon3Mainposition);
 	}
 
-	createCannon(x, y, z) {
+	createCannon(cannonFINAL, x, y, z) {
 		const cannonCylinderGeometry = new THREE.CylinderGeometry(
 			...this.cannonMainSize
 		);
@@ -50,19 +52,21 @@ export default class Cannon extends THREE.Group {
 		);
 		cannonCylinder.position.set(x, y, z);
     cannonCylinder.rotateZ(Math.PI / 2);
-		this.add(cannonCylinder);
+		cannonCylinder.add(new THREE.AxesHelper(15));
+		cannonFINAL.add(cannonCylinder);
 
     const xTire = this.cannonMainSize[1] / 2;
     const zTire = this.cannonMainSize[1];
 		const cannonTire1Geometry = new THREE.TorusGeometry(...this.cannonTireSize);
 		const cannonTire1 = new THREE.Mesh(cannonTire1Geometry, this.torusMaterial);
 		cannonTire1.position.set(x + xTire, y, z + zTire);
-		this.add(cannonTire1);
+		cannonFINAL.add(cannonTire1);
 
     const cannonTire2Geometry = new THREE.TorusGeometry(...this.cannonTireSize);
 		const cannonTire2 = new THREE.Mesh(cannonTire2Geometry, this.torusMaterial);
 		cannonTire2.position.set(x + xTire, y, z - zTire);
-		this.add(cannonTire2);
+		cannonFINAL.add(cannonTire2);
+		this.add(cannonFINAL);
 	}
 
 	update() {//alterar a cor em vez do wireframe para os cannons
