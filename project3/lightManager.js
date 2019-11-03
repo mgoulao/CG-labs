e7bd55242556625106ca615beb660eee7db33d5e
OWpLess <td-silva@hotmail.com>
2019-11-03 18:00:15 +0000

    q

--- project3/lightManager.js
+++ project3/lightManager.js
@@ -4,9 +4,10 @@ export default class LightManager {
 	constructor(scene) {
 		this.scene = scene;
 
-
-		const light = new THREE.AmbientLight(0xffffff); // soft white light
-		this, scene.add(light);
+		this.light = new THREE.DirectionalLight(0xff0000, 1); // soft white light
+		this.light.position.set(0,1,0);
+		this.light.target=scene.isocahedron;
+		this.add(this.light.target);
 	}
 
 	update() {}
