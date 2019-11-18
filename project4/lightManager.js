7585d55d9ce098e54b0b093af8d7bf354e89c164
Cruziper <goncalo.garcia.cruz@tecnico.ulisboa.pt>
2019-11-18 19:50:06 +0000

    first commit

--- project4/lightManager.js
+++ project4/lightManager.js
@@ -3,30 +3,98 @@ export default class LightManager {
 	constructor(scene) {
 		this.scene = scene;
 
+		this.acceleration = 0.01;
+		this.initialVelocity = 0.05;
+		this.maxROTATIONVelocity = 4;
+		this.currentROTATIONVelocity = 0;
+
 		this.ambientLight = new THREE.DirectionalLight(0xffffff, 1); // soft white ambientLight
 		this.ambientLight.position.set(0, 50, 10);
 		this.ambientLight.visible = true;
 		this.scene.add(this.ambientLight);
 
 		this.sphereGeometry = new THREE.SphereBufferGeometry(1, 16, 8);
-		this.pointLightColor = 0xff00ff;
+		this.pointLightColorR = 0xff0000;
+		this.pointLightColorG = 0x00ff00;
+		this.pointLightColorB = 0x0000ff;
 
-		this.pointLight = new THREE.PointLight(this.pointLightColor, 1, 200, 3);
-		this.pointLight.add(
+		this.pointLightR = new THREE.PointLight(this.pointLightColorR, 1, 200, 3);
+		this.pointLightR.add(
 			new THREE.Mesh(
 				this.sphereGeometry,
-				new THREE.MeshBasicMaterial({ color: this.pointLightColor })
+				new THREE.MeshBasicMaterial({ color: this.pointLightColorR })
 			)
 		);
-		this.pointLight.position.set(30, 30, -10);
-		this.pointLight.visible = true;
-		this.scene.add(this.pointLight);
+		this.pointLightR.position.set(0, 60, 0);
+		this.pointLightR.visible = true;
+		this.rotationPivotR = new THREE.Object3D();
+		this.rotationPivotR.add(this.pointLightR);
+		this.scene.add(this.rotationPivotR);
+
+		this.pointLightG = new THREE.PointLight(this.pointLightColorG, 1, 200, 3);
+		this.pointLightG.add(
+			new THREE.Mesh(
+				this.sphereGeometry,
+				new THREE.MeshBasicMaterial({ color: this.pointLightColorG })
+			)
+		);
+		this.pointLightG.position.set(30, 10, 0);
+		this.pointLightG.visible = true;
+		this.rotationPivotG = new THREE.Object3D();
+		this.rotationPivotG.add(this.pointLightG);
+		this.scene.add(this.rotationPivotG);
+
+		this.pointLightB = new THREE.PointLight(this.pointLightColorB, 1, 200, 3);
+		this.pointLightB.add(
+			new THREE.Mesh(
+				this.sphereGeometry,
+				new THREE.MeshBasicMaterial({ color: this.pointLightColorB })
+			)
+		);
+		this.pointLightB.position.set(0, 60, 0);
+		this.pointLightB.visible = true;
+		this.rotationPivotB = new THREE.Object3D();
+		this.rotationPivotB.add(this.pointLightB);
+		this.scene.add(this.rotationPivotB);
+
+
 	}
 
+	animations() {
+		if (this.scene.IN_MOTION) {
+			if (this.currentROTATIONVelocity == 0) {
+				this.currentROTATIONVelocity = this.initialVelocity;
+			}
+			if (this.currentROTATIONVelocity < this.maxROTATIONVelocity) {
+				this.currentROTATIONVelocity +=
+					this.currentROTATIONVelocity * this.acceleration;
+				if (this.currentROTATIONVelocity >= this.maxROTATIONVelocity) {
+					this.currentROTATIONVelocity = this.maxROTATIONVelocity;
+				}
+				console.log(this.currentROTATIONVelocity);
+			}
+		} else {
+			if (this.currentROTATIONVelocity > 0) {
+				this.currentROTATIONVelocity -=
+					this.currentROTATIONVelocity * this.acceleration;
+				if (this.currentROTATIONVelocity < this.initialVelocity) {
+					this.currentROTATIONVelocity = 0;
+				}
+				console.log(this.currentROTATIONVelocity);
+			}
+		}
+		this.rotationPivotR.rotation.x -= this.currentROTATIONVelocity / 100;
+		this.rotationPivotG.rotation.y -= this.currentROTATIONVelocity / 100;
+		this.rotationPivotB.rotation.z -= this.currentROTATIONVelocity / 100;
+		//this.ball.rotateOnAxis(this.rotationAxes, Math.PI/8);
+	}
 	reset() {}
 
 	update() {
-		this.pointLight.visible = this.scene.POINTLIGHT;
+		if (!this.scene.STOP_ANIMATIONS) this.animations();
+		this.pointLightR.visible = this.scene.POINTLIGHT;
+		this.pointLightG.visible = this.scene.POINTLIGHT;
+		this.pointLightB.visible = this.scene.POINTLIGHT;
 		this.ambientLight.visible = this.scene.AMBIENT_LIGHT;
 	}
 }
